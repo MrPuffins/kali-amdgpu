4fba20b0ddce2ac4f98ecf3d108626e7e6626588
Charles Mclean <46691980+MrPuffins@users.noreply.github.com>
2024-03-26 01:30:00 +0000

    Update install.sh
    version 1.0

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-wget https://repo.radeon.com/amdgpu-install/23.40.2/ubuntu/jammy/amdgpu-install_6.0.60002-1_all.deb
-sudo apt install -y ./amdgpu-install_6.0.60002-1_all.deb hashcat
-sed -i 's/ubuntu|linuxmint|debian)/ubuntu|linuxmint|debian|kali)/g' /usr/bin/amdgpu-install
+set -e
+PKG_NAME="amdgpu-install_6.0.60002-1_all.deb"
+PKG_URL="https://repo.radeon.com/amdgpu-install/23.40.2/ubuntu/jammy/$PKG_NAME"
+
+# Download AMDGPU package
+wget "$PKG_URL" -O "$PKG_NAME"
+
+# Install AMDGPU package
+sudo apt install -y ./"$PKG_NAME" hashcat
+
+# Modify installation script for Kali Linux compatibility
+sudo sed -i 's/ubuntu|linuxmint|debian)/&|kali/' /usr/bin/amdgpu-install
+
+# Install
 amdgpu-install --no-dkms --usecase=opencl
+
+# Display Hashcat information
 hashcat -I
+
+# Clean up downloaded package
+rm "$PKG_NAME"
